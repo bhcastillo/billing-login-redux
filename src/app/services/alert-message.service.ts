262c0a72b995f3e5183350c48122a71c4522d2ed
Brayan Castillo <bhcastillor@gmail.com>
2020-10-17 12:36:58 -0500

    add header component

--- src/app/services/alert-message.service.ts
+++ src/app/services/alert-message.service.ts
@@ -16,40 +16,40 @@ export class AlertMessageService  {
     });
   }
 
-  // messageDetailInvoices(){
-  //   Swal.fire({
-  //     icon: 'info',
-  //     title: 'Facturación',
-  //     html: `Usted tiene <b>${this.invoices.length}</b> 
-  //           facturas en mora, la suma total equivale a <b>COP${this.getTotalValues()}</b>`,
-  //   });
+  messageDetailInvoices(){
+    Swal.fire({
+      icon: 'info',
+      title: 'Facturación',
+      html: `Usted tiene <b>${this.invoices.length}</b> 
+            facturas en mora, la suma total equivale a <b>COP${this.getTotalValues()}</b>`,
+    });
     
-  // }
-  // messageNoPay(){
-  //    Swal.fire({
-  //       icon: 'question',
-  //       title: 'Cuentanos por que no puedes realizar el pago',
-  //       input: 'textarea',
-  //       inputAttributes: {
-  //         autocapitalize: 'off'
-  //       },
-  //       confirmButtonText:'Enviar',
-  //       inputValidator: (value) => {
-  //         if (!value) {
-  //         return '¡Necesitas escribir algo!'
-  //         }
-  //       }
-  //     }).then((result)=>{
-  //       if (result.isConfirmed){
-  //         Swal.fire({
-  //           icon:'success',
-  //           title:'Mensaje enviado con exito',
-  //           text: 'Esperamos tengas un excelente día',
-  //           timer:2000
-  //         })
-  //       }
-  //     })
-  // }
+  }
+  messageNoPay(){
+     Swal.fire({
+        icon: 'question',
+        title: 'Cuentanos por que no puedes realizar el pago',
+        input: 'textarea',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        confirmButtonText:'Enviar',
+        inputValidator: (value) => {
+          if (!value) {
+          return '¡Necesitas escribir algo!'
+          }
+        }
+      }).then((result)=>{
+        if (result.isConfirmed){
+          Swal.fire({
+            icon:'success',
+            title:'Mensaje enviado con exito',
+            text: 'Esperamos tengas un excelente día',
+            timer:2000
+          })
+        }
+      })
+  }
   getTotalValues(){
     const total = this.invoices.map(invoice=> invoice.value).reduce((acc,value)=>acc + value , 0);
     return new Intl.NumberFormat('co-ES').format(total);
